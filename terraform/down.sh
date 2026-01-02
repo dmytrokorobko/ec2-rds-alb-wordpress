b6bb5883190f9fcf74efddfa4cd6b863ea52cff9
Dmytro Korobko <dmytro.korobko@gmail.com>
2026-01-01 22:35:40 -0500

    Add lifecycle scripts

--- terraform/down.sh
+++ terraform/down.sh
@@ -1,1 +1,6 @@
 #!/bin/bash
+
+terraform -chdir=./04_rds/ destroy
+terraform -chdir=./03_alb/ destroy
+terraform -chdir=./02_compute/ destroy
+terraform -chdir=./01_network/ destroy
--- terraform/up.sh
+++ terraform/up.sh
@@ -1,1 +1,6 @@
 #!/bin/bash
+
+terraform -chdir=./01_network/ apply
+terraform -chdir=./02_compute/ apply
+terraform -chdir=./03_alb/ apply
+terraform -chdir=./04_rds/ apply
