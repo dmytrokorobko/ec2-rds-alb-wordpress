92aeba0403f8b085c9a08155a24267801018c19a
Dmytro Korobko <dmytro.korobko@gmail.com>
2026-01-01 22:34:57 -0500

    Configure Ansible connectivity via bastion and dynamic inventory

--- ansible/inventory/inventory.sh
+++ ansible/inventory/inventory.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
 
+set -e
+
+if [[ "$1" == "--list" ]]; then
+   IPLIST=$(terraform -chdir="../terraform/02_compute" output -json wordpress_ec2_private_ip)
+   jq -n --argjson ips "$IPLIST" '{
+      servers: {
+         hosts: $ips
+      }
+   }'
+else
+   echo "{}"
+fi
